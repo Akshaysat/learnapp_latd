d639cb985addf77b08e775aad602645ada66c6c4
Akshaysat <akshay.satpaise99@gmail.com>
2022-11-30 11:06:32 +0530

    updated app.py

--- app.py
+++ app.py
@@ -142,9 +142,6 @@ def workshop_container(day_no, date, workshop_name, workshop_jpeg, agenda, zoom_
         st.write("")
         st.write("")
 
-        st.write(dt.datetime.now())
-        st.write(cutoff_datetime)
-
         if dt.datetime.now() + dt.timedelta(hours=5, minutes=30) > cutoff_datetime:
             st.write("🤷 This live class is now over!")
         else:
